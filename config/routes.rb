c581bc62462a63c49090798772dd2297d15110ad
Jonathan <bklynbest718@gmail.com>
2018-04-24 18:49:02 -0400

    5:1

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
 
   get 'simple_pages/contact'
 
-  root 'simple_pages#landing_page'
+  root 'simple_pages#index'
   get 'simple_pages/index'
 
 resources :orders, only: [:index, :show, :create, :destroy]
