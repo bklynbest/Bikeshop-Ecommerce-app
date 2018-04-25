3a132b94022405d32fed7a081b77cc6da37c7a91
Jonathan <bklynbest718@gmail.com>
2018-04-25 19:52:06 -0400

    5:3

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
 
   root 'simple_pages#landing_page'
   get 'simple_pages/index'
-
+  post 'simple_pages/thank_you'
 resources :orders, only: [:index, :show, :create, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
