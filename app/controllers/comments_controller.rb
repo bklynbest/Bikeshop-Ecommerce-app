646683fa897c64df1fbb1c5166e88a4308d7de8d
Jonathan <bklynbest718@gmail.com>
2018-05-01 16:42:22 -0400

    5:8

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,7 @@
 class CommentsController < ApplicationController
   def create
     @product = Product.find(params[:product_id])
-    @comment - @product.comments.new(comment_params)
+    @comment = @product.comments.new(comment_params)
     @comment.user = current_user
     @comment.save
     redirect_to product_path(@product)
@@ -13,6 +13,6 @@ class CommentsController < ApplicationController
 
   private
     def comment_params
-      params.require(:comment).permit(:user_id, :body, :rating)      
+      params.require(:comment).permit(:user_id, :body, :rating)
     end
 end
