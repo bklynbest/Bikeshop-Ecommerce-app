03186bdedcc59494f43d7a306d33565995c7561f
Jonathan <bklynbest718@gmail.com>
2018-05-10 06:19:34 -0400

    6:6

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -1,28 +1,31 @@
 class PaymentsController < ApplicationController
   def create
-    @product = product.find(params[:product_id])
+    @product = Product.find(params[:product_id])
     @user = current_user
-  token = params [:stripeToken]
-  #create the charge on Stripes servers - this will charge the user's card
-  begin
-    charge = Stripe::Charge.create(
-      amount: @product.price, # amount in cents, again
-      currency: "usd",
-      source: token,
-      description: params[:stripeEmail]
-      :receipt_email => params[:stripeEmail]
-    )
+    token = params[:stripeToken]
+    # Create the charge on Stripe's servers - this will charge the user's card
+    begin
+      charge = Stripe::Charge.create(
+        amount: (@product.price * 100).to_i, # amount in cents, again
+        currency: "usd",
+        description: params[:stripeEmail],
+        source: token,
+      )
+
     if charge.paid
-      Order.create(product_id: @product.id, user_id: @user.id, total: @product.price)
+      Order.create(
+        product_id: @product.id,
+        user_id: @user_id,
+        total: @product.price
+      )
     end
-  rescue Stripe::CardError => e
-    # The card has been declined
-    body = e.json_body
-    err = body[:error]
-    flash[:error] = "Unfortunately, there was an error processing your payment: #{err[:message]}"
-end
-redirect_to product_path(@product)
 
-
-
+    rescue Stripe::CardError => e
+      # The card has been declined
+      body = e.json_body
+      err = body[:error]
+      flash[:error] = "Unfortunately, there was an error processing your payment: #{err[:message]}"
+    end
+    redirect_to simple_pages_payment_thank_you_path
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,9 @@ Rails.application.routes.draw do
 
   root 'simple_pages#landing_page'
   get 'simple_pages/index'
+  get 'simple_pages/payment_thank_you'
   post 'simple_pages/thank_you'
+  post 'payments/create'
 resources :orders, only: [:index, :show, :create, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
