8678e9a380b7aefa438c2d954bfc918a47d28c28
Jonathan <bklynbest718@gmail.com>
2018-05-17 08:03:19 -0400

    6:13

--- config/routes.rb
+++ config/routes.rb
@@ -14,5 +14,6 @@ Rails.application.routes.draw do
   post 'simple_pages/thank_you'
   post 'payments/create'
 resources :orders, only: [:index, :show, :create, :destroy]
+mount ActionCable.server => '/cable'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
