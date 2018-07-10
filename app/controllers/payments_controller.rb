51caa0b28bfdea1c5ea915cd377f494c222c5b12
Jonathan <bklynbest718@gmail.com>
2018-07-10 16:53:56 -0400

    updated stripe payments controller

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -1,34 +1,30 @@
 class PaymentsController < ApplicationController
-
-    def create
-        @product = Product.find(params[:product_id])
-        @user = current_user
-
-        token = params[:stripeToken]
-        begin
-            charge = Stripe::Charge.create(
-                amount: (@product.price*100).to_i,
-                currency: "gbp",
-                source: token,
-                description: params[:stripeEmail]
-            )
-
-        if charge.paid
-            Order.create(
-              product_id: @product.id,
-              user_id: @user.id,
-              total: @product.price
-            )
-            flash[:success] = "Your payment was processed successfully"
-        end
-
-        rescue Stripe::CardError => e
-            body = e.json_body
-            err = body[:error]
-            flash[:error] = "Unfortunately, there was an error processing your payment: #{err[:message]}"
-        end
-
-        redirect_to product_path(@product)
-
+  before_action :authenticate_user!
+  require 'stripe'
+  # payment token submitted with other data to this action.
+  def create
+    @product = Product.find(params[:product_id])
+    @user = current_user
+    token = params[:stripeToken]
+    # (begin-rescue) block for exception handling
+    begin
+      charge = Stripe::Charge.create(
+        amount:  (@product.price * 100).to_i,
+        currency: "usd",
+        source: token,
+        description: params[:stripeEmail],
+        :receipt_email => @user.email
+      )
+      if charge.paid
+        Order.create(product_id: @product.id, user_id: @user.id, total: @product.price)
+        flash[:notice] = "Enjoy your #{@product.name}"
+        UserMailer.payment_confirmation(@user, @product).deliver_now
+      end
+    rescue Stripe::CardError => e
+      body = e.json_body
+      err = body[:error]
+      flash[:error] = "Unfortunately, there was an error processing your payment: #{err[:message]}"
     end
+    redirect_to product_path(@product), notice: "#{@product.name} has been purchased successfully!"
+  end
 end
