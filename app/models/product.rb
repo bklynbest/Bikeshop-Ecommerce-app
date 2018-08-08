928d6962951887487a58772bd20f56f078dd7a30
Jonathan <bklynbest718@gmail.com>
2018-08-08 00:22:53 -0400

    added redis cache for products

--- app/models/product.rb
+++ app/models/product.rb
@@ -24,20 +24,21 @@ class Product < ApplicationRecord
       Product.where("name ilike ?", "%#{search_term}%")
     end
   end
+  
   def views
-     $redis.get("product:#{id}")
+  	$redis.get("product:#{id}")
   end
 
   def viewed
-     $redis.incr("product:#{id}")
+  	$redis.incr("product:#{id}")
   end
 
   def set_recent_reviewer(user_name)
-     $redis.set("rr_product:#{id}","#{user_name}")
+  	$redis.set("rr_product:#{id}","#{user_name}")
   end
 
   def get_recent_reviewer
-     $redis.get("rr_product:#{id}")
+  	$redis.get("rr_product:#{id}")
   end
 
 end
