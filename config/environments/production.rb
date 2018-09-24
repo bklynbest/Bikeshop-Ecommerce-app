88ac94a88c9e6780b6026e9edb145fc4e7e460f4
Jonathan <bklynbest718@gmail.com>
2018-09-24 06:02:49 -0400

    compile assets bug test

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -24,17 +24,18 @@ config.webpacker.check_yarn_integrity = false
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
-  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
+  config.public_file_server.enabled = true
 
   # Compress JavaScripts and CSS.
+
   config.assets.js_compressor = Uglifier.new(harmony: true)
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = true
+  config.assets.compile = false
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
-  config.assets.precompile = ['.js', '.css', '*.css.erb']
+  config.assets.precompile = ['.js', '.css', '*.css.erb', 'scss']
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = 'http://assets.example.com'
